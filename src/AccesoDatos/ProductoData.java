0348da5be713caf19caf67e5bde1004d1c37965a
Mariano1498 <barrosomariano98@gmail.com>
2023-10-02 16:59:56 -0300

    Create ProductoData.java

---
+++ src/AccesoDatos/ProductoData.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccesoDatos;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Mariano
+ */
+public class ProductoData {
+    private Connection con = null;
+}
