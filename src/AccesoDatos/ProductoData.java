d77aa48f6b21deee4582590d38bd3a3bd38dcaa4
Mariano1498 <barrosomariano98@gmail.com>
2023-10-02 17:12:21 -0300

    Update ProductoData.java

--- src/AccesoDatos/ProductoData.java
+++ src/AccesoDatos/ProductoData.java
@@ -6,6 +6,8 @@
 package AccesoDatos;
 
 import java.sql.*;
+import Entidades.Producto;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,4 +15,28 @@ import java.sql.*;
  */
 public class ProductoData {
     private Connection con = null;
+
+    public ProductoData() {
+        con = Conexion.getConexion();
+    }
+    
+    public void guardarProducto(Producto producto){
+        String sql = "INSERT INTO producto (nombreProducto, descripcion, precioActual, stock, estado) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, producto.getNombreProducto());
+            ps.setString(2, producto.getDescripcion());
+            ps.setDouble(3, producto.getPrecioActual());
+            ps.setInt(4, producto.getStock());
+            ps.setBoolean(5, producto.isEstado());
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                producto.setIdProducto(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Producto añadido con exito.");
+            }
+            ps.close();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Producto" + ex.getMessage());
+        }
+    }
 }
