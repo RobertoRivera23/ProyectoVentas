58a83a929881d46ec511806385074004a2910d81
Diosito00 <pikasensual.kuro@gmail.com>
2023-10-02 16:46:37 -0300

    entidades

---
+++ src/Entidades/DetalleVenta.java
@@ -0,0 +1,75 @@
+package Entidades;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Diosito
+ */
+public class DetalleVenta {
+    private int idDetalleVenta, cantidad;
+    private Venta venta;
+    private double precioVenta;
+    private Producto producto;
+
+    public DetalleVenta(int cantidad, Venta venta, double precioVenta, Producto producto) {
+        this.cantidad = cantidad;
+        this.venta = venta;
+        this.precioVenta = precioVenta;
+        this.producto = producto;
+    }
+
+    public DetalleVenta(int idDetalleVenta, int cantidad, Venta venta, double precioVenta, Producto producto) {
+        this.idDetalleVenta = idDetalleVenta;
+        this.cantidad = cantidad;
+        this.venta = venta;
+        this.precioVenta = precioVenta;
+        this.producto = producto;
+    }
+
+    public DetalleVenta() {
+    }
+
+    public int getIdDetalleVenta() {
+        return idDetalleVenta;
+    }
+
+    public void setIdDetalleVenta(int idDetalleVenta) {
+        this.idDetalleVenta = idDetalleVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
---
+++ src/Entidades/Venta.java
@@ -0,0 +1,57 @@
+package Entidades;
+
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Diosito
+ */
+public class Venta {
+
+    private int idVenta;
+    private Cliente cliente;
+    private LocalDate fechaVenta;
+
+    public Venta(Cliente cliente, LocalDate fechaVenta) {
+        this.cliente = cliente;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Venta(int idVenta, Cliente cliente, LocalDate fechaVenta) {
+        this.idVenta = idVenta;
+        this.cliente = cliente;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Venta() {
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+}
