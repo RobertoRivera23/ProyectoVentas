65540ded194638747447b13a582126190d7bb086
Diosito00 <pikasensual.kuro@gmail.com>
2023-10-04 16:11:40 -0300

    listaCliente

--- src/AccesoDatos/ClienteData.java
+++ src/AccesoDatos/ClienteData.java
@@ -6,6 +6,8 @@ package AccesoDatos;
 
 import Entidades.Cliente;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -102,5 +104,27 @@ public class ClienteData {
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Cliente " + ex.getMessage());
         }
+    }
+    
+    public List<Cliente> listarClientes(){
+        List<Cliente> listaC = new ArrayList<>();
+        String sql = "SELECT * FROM cliente ORDER BY idCliente ASC";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                cliente = new Cliente();
+                cliente.setIdCliente(rs.getInt("idCliente"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setDomicilio(rs.getString("domicilio"));
+                cliente.setTelefono(rs.getString("telefono"));
+                listaC.add(cliente);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la table Cliente." + ex.getMessage());
+        }
+        return listaC;
     }
 }
