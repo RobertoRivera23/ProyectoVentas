43970e02d9e0e1ebbfae1a0dc704a4d7a41d9a4d
Diosito00 <pikasensual.kuro@gmail.com>
2023-10-04 15:23:50 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+
+import AccesoDatos.ProductoData;
+import Entidades.Producto;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +18,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        ProductoData p1 = new ProductoData();
+//        Producto p = p1.buscarProducto(1);
+//        p.setNombreProducto("Heladera");
+//        p.setPrecioActual(600000);
+//        p1.modificarProducto(p);
+//        Producto p2 = new Producto(50, "Horno Electrico", "Electrico", 200000, true);
+//        p1.guardarProducto(p2);
+//        p1.eliminarProducto(1);
     }
     
 }
