1279a1794466e0d8a55196d4297a7f555f3648cc
Diosito00 <pikasensual.kuro@gmail.com>
2023-10-10 16:23:19 -0300

    Revert "Merge branch 'main' of https://github.com/RobertoRivera23/ProyectoVentas"
    This reverts commit 5358bfec4db43109930be12769f071e98b2d6019, reversing
    changes made to aa1a4e50bcbbd309b5fed0ca7c41d93478129814.

---
+++ src/AccesoDatos/ClienteData.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AccesoDatos;
+
+import Entidades.Cliente;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Diosito
+ */
+public class ClienteData {
+
+    private Connection con = null;
+    private Cliente cliente;
+
+    public ClienteData() {
+        con = Conexion.getConexion();
+    }
+
+    public void guardarCliente(Cliente cliente) {
+        String sql = "INSERT INTO cliente (apellido, nombre, domicilio, telefono, estado) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, cliente.getApellido());
+            ps.setString(2, cliente.getNombre());
+            ps.setString(3, cliente.getDomicilio());
+            ps.setString(4, cliente.getTelefono());
+            ps.setBoolean(5, cliente.isEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                cliente.setIdCliente(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Se ha añadido al cliente con exito.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Cliente. " + ex.getMessage());
+        }
+    }
+
+    public Cliente buscarCliente(int idCliente) {
+        String sql = "SELECT apellido, nombre, domicilio, telefon FROM cliente WHERE idCliente = ? AND estado = 1";
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idCliente);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                cliente = new Cliente();
+                cliente.setIdCliente(idCliente);
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setDomicilio(rs.getString("domicilio"));
+                cliente.setTelefono(rs.getString("telefono"));
+                cliente.setEstado(rs.getBoolean("estado")); //  no deberiamos setear el estado al crearlo? aca estamos buscando uno que ya esta
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el Cliente.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Cliente. " + ex.getMessage());
+        }
+        return cliente;
+    }
+
+    public void modicifarCliente(Cliente cliente) {
+        String sql = "UPDATE cliente SET apellido = ? , nombre = ? , domicilio = ? , telefono = ? WHERE idCliente = ?";
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getApellido());
+            ps.setString(2, cliente.getNombre());
+            ps.setString(3, cliente.getDomicilio());
+            ps.setString(4, cliente.getTelefono());
+            ps.setInt(5, cliente.getIdCliente());
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "El Cliente no existe.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Cliente. " + ex.getMessage());
+        }
+    }
+
+    public void eliminarCliente(int id) {
+        String sql = "UPDATE cliente SET estado = 0 WHERE idCliente = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int rs = ps.executeUpdate();
+            if (rs == 1) {
+                JOptionPane.showMessageDialog(null, "Cliente eliminado.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Cliente " + ex.getMessage());
+        }
+    }
+    
+    public List<Cliente> listarClientes(){
+        List<Cliente> listaC = new ArrayList<>();
+        String sql = "SELECT * FROM cliente WHERE estado = 1 ORDER BY idCliente ASC";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                cliente = new Cliente();
+                cliente.setIdCliente(rs.getInt("idCliente"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setDomicilio(rs.getString("domicilio"));
+                cliente.setTelefono(rs.getString("telefono"));
+                listaC.add(cliente);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la table Cliente." + ex.getMessage());
+        }
+        return listaC;
+    }
+}
---
+++ src/Entidades/Venta.java
@@ -0,0 +1,83 @@
+package Entidades;
+
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Diosito
+ */
+public class Venta {
+
+    private int idVenta;
+    private Cliente cliente;
+    private Empleado empleado;
+    private LocalDate fechaVenta;
+    private boolean estado;
+
+    public Venta(Cliente cliente, Empleado empleado, LocalDate fechaVenta) {
+        this.cliente = cliente;
+        this.empleado = empleado;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Venta(int idVenta, Cliente cliente, Empleado empleado, LocalDate fechaVenta) {
+        this.idVenta = idVenta;
+        this.cliente = cliente;
+        this.empleado = empleado;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Venta() {
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+    
+    public Integer getIdCliente(){
+        return cliente.getIdCliente();
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+    
+    
+}
