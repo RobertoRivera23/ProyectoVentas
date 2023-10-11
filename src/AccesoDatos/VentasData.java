c1ee058b4bb217f85f76575944471a5052e90fe5
Diosito00 <pikasensual.kuro@gmail.com>
2023-10-11 16:48:39 -0300

    Update VentasData.java

--- src/AccesoDatos/VentasData.java
+++ src/AccesoDatos/VentasData.java
@@ -53,11 +53,9 @@ public class VentasData {
     public void setEmpleado(Empleado empleado) {
         this.empleado = empleado;
     }
-    
-    
 
 // Guardar ventas
-    public void guardarVenta(Venta venta) { 
+    public void guardarVenta(Venta venta) {
         String sql = "INSERT INTO venta (idCliente, idempleado, fechaVenta, estado ) VALUES (? , ?, ?, ?) ";
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -65,7 +63,7 @@ public class VentasData {
             ps.setInt(2, venta.getEmpleado().getIdEmpleado());
             ps.setDate(3, Date.valueOf(venta.getFechaVenta()));
             ps.setBoolean(4, true);
-           ps.executeUpdate();
+            ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
                 venta.setIdVenta(rs.getInt(1));
@@ -196,9 +194,32 @@ public class VentasData {
         }
         return prodPorFecha;
     }
-    
-    public Venta buscarVentaId(int idVenta){
-        Venta venta = new Venta();
+
+    public Venta buscarVentaId(int idVenta) {
+        String sql = "SELECT idCliente, idEmpleado, fechaVenta, estado FROM venta "
+                + "WHERE idVenta = ? AND estado = 1";
+        ClienteData c1 = new ClienteData();
+        EmpleadoData e1 = new EmpleadoData();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1,idVenta);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                venta = new Venta();
+                venta.setIdVenta(idVenta);
+                cliente = c1.buscarCliente(rs.getInt("idCliente"));
+                venta.setCliente(cliente);
+                empleado = e1.buscarEmpleadoPorId(rs.getInt("idEmpleado"));
+                venta.setEmpleado(empleado);
+                venta.setFechaVenta(rs.getDate("fechaVenta").toLocalDate());
+                venta.setEstado(rs.getBoolean("estado"));
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe la Venta");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Venta " + ex.getMessage());
+        }
         return venta;
     }
 }
