2e96ee619667339a1b7eeca779860a770721a648
Mariano1498 <barrosomariano98@gmail.com>
2023-10-12 17:41:23 -0300

    añ

--- src/Vistas/Menu.java
+++ src/Vistas/Menu.java
@@ -13,6 +13,7 @@ import Utilidades.TablaFraveMax;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
 
@@ -1269,6 +1270,9 @@ public class Menu extends javax.swing.JFrame {
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
                 jLBtnAgregarProdMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jLBtnAgregarProdMousePressed(evt);
             }
         });
         jtpAgregarPro.add(jLBtnAgregarProd, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 350, 80, 30));
@@ -2574,6 +2578,31 @@ public class Menu extends javax.swing.JFrame {
     private void jTFNombreClienteAgrClienKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTFNombreClienteAgrClienKeyTyped
         controlLetras(evt);
     }//GEN-LAST:event_jTFNombreClienteAgrClienKeyTyped
+
+    private void jLBtnAgregarProdMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLBtnAgregarProdMousePressed
+        if(jTFNombreProductoAgrProd.getText().trim().isEmpty() || jTFPrecioActualAgrProd.getText().trim().isEmpty()
+                || jTFDescripcionAgrProd.getText().trim().isEmpty() || jTFStockAgrProd.getText().trim().isEmpty()){
+            JOptionPane.showMessageDialog(this, "No puede haber campos vacios");
+        }else{
+            try{
+                ProductoData productoD = new ProductoData();
+                Producto producto = productoD.buscarProductoPorNombre(jTFNombreProductoAgrProd.getText());
+                if(producto == null){
+                    Producto product = new Producto();
+                    product.setNombreProducto(jTFNombreProductoAgrProd.getText());
+                    product.setDescripcion(jTFDescripcionAgrProd.getText());
+                    product.setPrecioActual(Double.parseDouble(jTFPrecioActualAgrProd.getText()));
+                    product.setStock(Integer.parseInt(jTFStockAgrProd.getText()));
+                    product.setEstado(true);
+                    productoD.guardarProducto(product);
+                }else{
+                    JOptionPane.showMessageDialog(this, "Ya hay un producto con ese nombre si quiere modificarlo vaya a la pestaña de modificar producto");
+                }
+            }catch(NullPointerException ex){
+                
+            }
+        }
+    }//GEN-LAST:event_jLBtnAgregarProdMousePressed
 
     /**
      * @param args the command line arguments
